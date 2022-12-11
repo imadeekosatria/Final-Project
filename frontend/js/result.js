50d7f7cf3f6d1e94bb59900c0fdcbdae0d4b51b0
imadeekosatria <imadeekosatriawiguna@gmail.com>
2022-12-11 21:50:22 +0800

    frontend

---
+++ frontend/js/result.js
@@ -0,0 +1,21 @@
+// const nameJS = document.getElementById('name');
+
+// console.log(nameJS.innerHTML);
+
+// const api_url = 'http://127.0.0.1:8000/get_result_json/'+nameJS.innerHTML;
+
+// async function getapi(url) {
+    
+//     // Storing response
+//     const response = await fetch(url);
+    
+//     // Storing data in form of JSON
+//     var data = await response.json();
+    
+//     if (response) {
+//         hideloader();
+//     }
+//     show(data);
+// }
+
+// getapi(api_url);
